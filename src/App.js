5c92bfc5c14a3285a62978e6f252f35b3165bc95
Waire <waire.tega@gmail.com>
2021-05-12 12:34:38 +0100

    error fixed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Converter from './components/Converter';
 import {FaArrowsAltV} from 'react-icons/fa';
 
 function App() {
-  const BASE_URL = "https://data.fixer.io/api/latest";
+  const BASE_URL = "http://data.fixer.io/api/latest?access_key=434653cdde5e98ee627ec2027917d78d&format=1";
 
   const [currOptions, setCurrOptions] = useState([]);
   const [fromCurr, setFromCurr] = useState();
@@ -12,17 +12,19 @@ function App() {
   const [amount, setAmount] = useState(1);
   const [amountInFromCurr, setAmountInFromCurr] = useState(true);
   const [exchangeRate, setExchangeRate] = useState();
+  const onChangeSelectedFromCurr = (e) => setFromCurr(e.target.value);
+  const onChangeSelectedToCurr = (e) => setToCurr(e.target.value);
 
   useEffect( () => {
     fetch(BASE_URL)
       .then(res => res.json())
       .then(data => {
         const firstCurr = Object.keys(data.rates)[0];
-        setCurrOptions([data.base, ...Object.keys(data.rates)])
-        console.log(data);
+        setCurrOptions([...Object.keys(data.rates)]);
         setFromCurr(data.base);
         setToCurr(firstCurr);
         setExchangeRate(data.rates[firstCurr])
+        // console.log(data);
       })
   },[])
 
@@ -34,26 +36,23 @@ useEffect(() => {
   }
 }, [fromCurr, toCurr])
 
-  const onChangeSelectedFromCurr = (e) => setFromCurr(e.target.value);
-  const onChangeSelectedToCurr = (e) => setToCurr(e.target.value);
 
   let toAmount, fromAmount;
   if (amountInFromCurr){
     fromAmount = amount;
-    toAmount = amount * exchangeRate;
+    toAmount = amount*exchangeRate;
   }else{
     toAmount = amount;
-    fromAmount = amount / exchangeRate;
+    fromAmount = amount/exchangeRate;
   }
   const handleFromAmountChange = (e) => {setAmount(e.target.value); setAmountInFromCurr(true)}
   const handleToAmountChange = (e) => {setAmount(e.target.value); setAmountInFromCurr(false)}
   
-
   return (
    <>
-   <Converter currOptions = {currOptions} selectedCurr = {fromCurr} onChangeSelectedCurr = {onChangeSelectedFromCurr} amount = {fromAmount} onChangeAmount = {handleFromAmountChange} />
+   <Converter currOptions = {currOptions} selectedCurr = {fromCurr} onChangeSelectedCurr = {onChangeSelectedFromCurr} theAmount = {fromAmount} onChangeAmount = {handleFromAmountChange} />
    <div className="arrow"><FaArrowsAltV /></div>
-   <Converter currOptions = {currOptions} selectedCurr = {toCurr} onChangeSelectedCurr = {onChangeSelectedToCurr} amount = {toAmount} onChangeAmount = {handleToAmountChange} />
+   <Converter currOptions = {currOptions} selectedCurr = {toCurr} onChangeSelectedCurr = {onChangeSelectedToCurr} theAmount = {toAmount} onChangeAmount = {handleToAmountChange} />
 
    </>
   );
--- src/components/Converter.js
+++ src/components/Converter.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Converter(props) {
     return (
         <>
-        <input type="number" value= {props.amount} onChange = {props.onChangeAmount} />
+        <input type="number" value={props.theAmount} onChange = {props.onChangeAmount} />
         <select name="" id="" value={props.selectedCurr} onChange = {props.onChangeSelectedCurr}>
             {props.currOptions.map(option => <option value={option} key={option}>{option}</option>)}
         </select>
